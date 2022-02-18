7b7d456b345604ed69943818f8cc3246645193bd
wahyu-hidayah <maryu2992@gmail.com>
2022-02-18 17:23:24 +0700

    perulangan dengan for untuk jumlah buku tertentu,contoh 10 buku

---
+++ Dasar_Sintaksis_Perulangan_baca_buku_dengan_for.py
@@ -0,0 +1,22 @@
+"""
+program perulangan mambaca buku dengan for
+"""
+jumlah_buku = 10
+print('ibu berkata, "baca semua bukumu"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f'jumlah buku yang sudah di baca {jumlah_buku_yang_sudah_dibaca}')
+
+print('dengan for')
+for jumlah_buku_yang_sudah_dibaca in range(1, jumlah_buku+1):
+    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah di baca")
+
+print(f'jumlah buku yang sudah di baca {jumlah_buku_yang_sudah_dibaca}')
+
+# tanpa for
+print('tanpa for')
+print('buku ke 1 sudah dibaca')
+print('buku ke 2 sudah dibaca')
+print('buku ke 3 sudah dibaca')
+print('buku ke 4 sudah dibaca')
+print('buku ke 5 sudah dibaca')
